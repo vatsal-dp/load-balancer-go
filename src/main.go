1a51f49eafea4e5e7d2285f52e36b788f49c18da
vatsal-dp <ptelvtsl@gmail.com>
2024-12-25 13:48:38 -0500

    basic roundrobin ready

--- src/main.go
+++ src/main.go
@@ -19,7 +19,7 @@ type simpleServer struct {
 	proxy *httputil.ReverseProxy
 }
 
-func newSimpleServer(addr string) *simpleServer {
+func NewSimpleServer(addr string) *simpleServer {
 	serverUrl, err := url.Parse(addr)
 
 	handleError(err)
@@ -51,12 +51,42 @@ func handleError(err error) {
 	}
 }
 
-func (lb *LoadBalancer) getNextAvailableServer() Server {
+func (s *simpleServer) Address() string { return s.addr }
 
+func (s *simpleServer) IsAlive() bool { return true }
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, req *http.Request) {
+	s.proxy.ServeHTTP(rw, req)
 }
 
-func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {}
+func (lb *LoadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	return server
+}
+
+func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Printf("forwarding request to address: %q\n", targetServer.Address())
+	targetServer.Serve(rw, r)
+}
 
 func main() {
+	servers := []Server{
+		NewSimpleServer("https://www.facebook.com"),
+		NewSimpleServer("https://www.google.com"),
+		NewSimpleServer("https://www.youtube.com"),
+	}
+	lb := newLoadBalancer("8000", servers)
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
+		lb.serveProxy(rw, req)
+	}
+	http.HandleFunc("/", handleRedirect)
 
+	fmt.Printf("serving requests at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
 }
